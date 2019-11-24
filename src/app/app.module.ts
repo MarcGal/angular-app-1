c7effc5a4cd8797c7fafd915098debf4bf6ec321
Mar <mgalloperalta@gmail.com>
2019-11-24 20:03:29 +0100

    Create new component using te ng g c command. App module automatically updates. new comp reutilized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 // we need to import newly created components here
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component'; // no need to add the ts extension
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component'; // no need to add the ts extension
 
 @NgModule({
   declarations: [
     AppComponent,
     // we need to import newly created components here
     ServerComponent,
+    ServersComponent,
   ],
   imports: [
     BrowserModule,
