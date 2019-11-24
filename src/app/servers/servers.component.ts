c23689a84476c08d4deb172ea5d4877d1a90f964
Mar <mgalloperalta@gmail.com>
2019-11-24 20:21:27 +0100

    Add different types of styles. Normal css, bootstrao classes, and inline styles inside the compoenent ts file

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // for both the template and styles we can write styling code in an inline manner.
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
