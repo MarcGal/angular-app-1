f1a5ba9c878fba30ddc52e83b76087cc4ce4dac7
Mar <mgalloperalta@gmail.com>
2019-11-24 19:52:37 +0100

    import server component into app the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+// we need to import newly created components here
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; // no need to add the ts extension
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // we need to import newly created components here
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
